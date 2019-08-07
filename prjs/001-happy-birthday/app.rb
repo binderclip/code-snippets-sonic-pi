b1ae8ea9614dab695cb49bae9d6c9780ed3f86c9
clip <binderclip2014@gmail.com>
2019-08-08 01:43:34 +0800

    update

--- prjs/001-happy-birthday/app.rb
+++ prjs/001-happy-birthday/app.rb
@@ -1,53 +1,55 @@
+x = 0.25
+
 play :g4
-sleep 0.25
+sleep x
 play :g4
-sleep 0.25
+sleep x
 play :a4
-sleep 0.5
+sleep x * 2
 play :g4
-sleep 0.5
+sleep x * 2
 play :c5
-sleep 0.5
+sleep x * 2
 play :b4
-sleep 1
+sleep x * 4
 
 play :g4
-sleep 0.25
+sleep x
 play :g4
-sleep 0.25
+sleep x
 play :a4
-sleep 0.5
+sleep x * 2
 play :g4
-sleep 0.5
+sleep x * 2
 play :d5
-sleep 0.5
+sleep x * 2
 play :c5
-sleep 1
+sleep x * 4
 
 play :g4
-sleep 0.25
+sleep x
 play :g4
-sleep 0.25
+sleep x
 play :g5
-sleep 0.5
+sleep x * 2
 play :e5
-sleep 0.5
+sleep x * 2
 play :c5
-sleep 0.5
+sleep x * 2
 play :b4
-sleep 0.5
+sleep x * 2
 play :a4
-sleep 0.75
+sleep x * 3
 
 play :f5
-sleep 0.25
+sleep x
 play :f5
-sleep 0.25
+sleep x
 play :e5
-sleep 0.5
+sleep x * 2
 play :c5
-sleep 0.5
+sleep x * 2
 play :d5
-sleep 0.5
+sleep x * 2
 play :c5
-sleep 1.5
+sleep x * 6
